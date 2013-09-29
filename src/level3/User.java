2d9e39ce6c313cda2108b4d1458d7d27e8ae485a
nekosyu <nekosyu@cosmos-xp>
2013-09-30 05:04:33 +0900

    level3追加

---
+++ src/level3/User.java
@@ -0,0 +1,30 @@
+package level3;
+
+public class User {
+
+	public static void main(String[] args) {
+		
+		//WindowsXPクラスをインスタンス化
+		WindowsXP os = new WindowsXP();
+
+		//Windows起動
+		os.start();
+		
+		//ログイン（ユーザ名/パスワードあり）
+		os.login("Taro", "kumamoto");
+		
+		//ウインドウを開くメッセージ
+		os.open();
+
+		//ウィンドウを閉じるメッセージ
+		os.close();
+		
+		//リモートデスクトップ接続
+		os.remoteDesktop("hanako");
+		
+		//WindowsXP終了
+		os.shutdown();
+
+	}
+
+}
