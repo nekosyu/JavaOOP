5a616de6ad676f52c5136ddcfeb4aa9ffc3c0061
nekosyu <nekosyu@KCL-W7x64>
2013-10-01 16:42:01 +0900

    level5追加

---
+++ src/level5/User.java
@@ -0,0 +1,25 @@
+package level5;
+
+public class User {
+
+	public static void main(String[] args) {
+		
+		//クラスのインスタンス化
+		Windows[] aryWindows ={ new Windows(),
+													new Windows(),
+													new WindowsXP(),
+													new WindowsXP(),
+													new WindowsXP(),
+													new WindowsXP(),
+													new WindowsVista(),
+													new WindowsVista(),
+													new WindowsVista(),
+													new WindowsServer()
+		};
+		
+		//Windowsの起動
+		for( int i = 0; i < 10 ; i++ ){
+			aryWindows[i].start();
+		}
+	}
+}
---
+++ src/level5/Windows.java
@@ -0,0 +1,35 @@
+package level5;
+
+public class Windows {
+
+	//フィールド
+	protected String user;
+	protected String password;
+
+	//Windows起動メソッド
+	public void start(){
+		System.out.println("Windowsを起動しました");
+	}
+	
+	//ログインメソッド
+	public void login(String inUser, String inPassword){
+		user = inUser;
+		password = inPassword;
+		System.out.println(user + "がログインしました");
+	}
+	
+	//ウィンドウ表示メソッド
+	public void open(){
+		System.out.println(user + "がウィンドウを表示しました");
+	}
+
+	//ウィンドウを閉じるメソッド
+	public void close(){
+		System.out.println(user + "がウィンドウを閉じました");
+	}
+	
+	//Windows終了メソッド
+	public void shutdown(){
+		System.out.println("Windowsを終了しました");
+	}
+}
