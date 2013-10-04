19cbf6a3c7cb1ed92126e59f421f0c379db69c49
nekosyu <nekosyu@KCL-W7x64>
2013-10-04 18:26:15 +0900

    level6変更

---
+++ src/level6/Software.java
@@ -0,0 +1,11 @@
+package level6;
+
+public interface Software {
+
+	//インストールメソッド（抽象メソッド）
+	void install();
+	
+	//アンインストールメソッド（抽象メソッド）
+	void uninstall();
+	
+}
---
+++ src/level6/Word.java
@@ -0,0 +1,17 @@
+package level6;
+
+public class Word implements Software {
+
+	@Override
+	public void install() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void uninstall() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
